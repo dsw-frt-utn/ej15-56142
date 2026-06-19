57f17e141d45bf4c2b655cdaa9180335e96fcfd8
lopezsardip <lopezsardip@gmail.com>
2026-06-19 13:16:08 -0300

    subir cambios para actualizar nombre al repo

---
+++ Dsw2026Ej15/Dsw2026Ej15.Api/Controllers/DoctorsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2026Ej15.Api.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class DoctorsController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateDoctor()
+        {
+            return Created();
+        }
+    }
+}
