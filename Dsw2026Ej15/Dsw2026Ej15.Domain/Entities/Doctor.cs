027516a72089cace09abdfedd2f5692375d17fda
Diego Luna <Diego.LunaMendoza@alu.frt.utn.edu.ar>
2026-06-19 11:29:08 -0300

    Crear clases del Domain

--- Dsw2026Ej15/Dsw2026Ej15.Domain/Entities/Doctor.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Domain/Entities/Doctor.cs
@@ -2,9 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Dsw2026Ej15.Domain
+namespace Dsw2026Ej15.Domain.Entities
 {
-    internal class Doctor
+    internal class Doctor : BaseEntity
     {
+        public string Name { get; init; }
+        public string LicenseNumber { get; init; }
+        public bool IsActive { get; private set; }
+        public Speciality? Speciality { get; private set; }
+        public Doctor (string name, string licenseNumber, Speciality speciality,Guid? id = null) : base (id)
+        {
+            Name = name;
+            LicenseNumber = licenseNumber;
+            Speciality = speciality;
+            IsActive = true;
+        }
     }
 }
