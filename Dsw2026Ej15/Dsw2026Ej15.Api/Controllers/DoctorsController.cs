5e3caa5c065ae1a8f16d9ca672d4483d00ab6439
Diego Luna <Diego.LunaMendoza@alu.frt.utn.edu.ar>
2026-06-19 13:15:54 -0300

    Subir cambios para cambiar nombre al repo

---
+++ Dsw2026Ej15/Dsw2026Ej15.Api/Controllers/DoctorsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2026Ej15.Api.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
